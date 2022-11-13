9640c8bd5b04faafc0ccf6f1d53b3ce2fb1366e3
AnxianZhang <gadanxianzhang@gmail.com>
2022-11-13 12:06:40 +0100

    nav bar added with a few styles and assets

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 // with rsc (react statless component) we can create all lignes bellow
 import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from './pages/About';
+import Home from './pages/Home';
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello react :D</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+
+        {/* path="*" is when there is an error (like 404) */}
+        <Route path="*" element={<h1>Error 404</h1>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Navigation></Navigation>
+            <h1>A props</h1>
+            <br />
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde quos nihil suscipit mollitia accusantium deleniti id fuga at, eum modi sapiente non ut delectus saepe possimus obcaecati. Ab quam aut a corporis! Beatae numquam cupiditate doloremque pariatur nostrum nisi nihil quod consequatur id, accusantium, ducimus dignissimos dolorum ea. Iste porro odit nesciunt molestias quas quam consectetur quia natus quae delectus, voluptatibus quo aliquam laborum vel aspernatur inventore, molestiae quod voluptatum, in nostrum placeat tenetur. Dolorum harum voluptates, praesentium cupiditate quis expedita ducimus illum hic, obcaecati consequatur quia laudantium. Distinctio deleniti explicabo praesentium optio nisi. Culpa vitae laborum consequatur doloribus id!</p>
+            <br />
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde quos nihil suscipit mollitia accusantium deleniti id fuga at, eum modi sapiente non ut delectus saepe possimus obcaecati. Ab quam aut a corporis! Beatae numquam cupiditate doloremque pariatur nostrum nisi nihil quod consequatur id, accusantium, ducimus dignissimos dolorum ea. Iste porro odit nesciunt molestias quas quam consectetur quia natus quae delectus, voluptatibus quo aliquam laborum vel aspernatur inventore, molestiae quod voluptatum, in nostrum placeat tenetur. Dolorum harum voluptates, praesentium cupiditate quis expedita ducimus illum hic, obcaecati consequatur quia laudantium. Distinctio deleniti explicabo praesentium optio nisi. Culpa vitae laborum consequatur doloribus id!</p>
+        </div>
+    );
+};
+
+export default About;
